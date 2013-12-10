c6517791091b5bc5a51f6ca58f9921dac22997f9
Kalovdutii <true.bilberry.pie@gmail.com>
2013-12-10 04:20:30 +0400

    YOPTA

--- src/ru/sarchan/yoba/models/VisitorsModel.java
+++ src/ru/sarchan/yoba/models/VisitorsModel.java
@@ -8,16 +8,29 @@ import java.util.ArrayList;
 import javax.swing.table.AbstractTableModel;
 import ru.sarchan.yoba.dataConnectors.DataConnectable;
 import ru.sarchan.yoba.dataConnectors.TestingDataConnector;
+import ru.sarchan.yoba.entity.Visitor;
 
 /**
  *
  * @author void
  */
-public class VisitorsModel {
-    private DataConnectable connector;
-    private ArrayList visitors;
+public class VisitorsModel extends AbstractModel{
+    private ArrayList<Visitor> visitors;
     
     public VisitorsModel() {
         connector =  new TestingDataConnector(this);
+        refresh();
+    }
+    
+    public void refresh() {
+        visitors = connector.getData();
+    }
+    
+    public ArrayList<Visitor> getData() {
+        return visitors;
+    }
+    
+    public double calculate() {
+        return 1.7;
     }
 }
